39f4d2d2222dd12b22cff5159a32884b2433af62
GemmaTuron <gemma@ersilia.io>
2024-09-23 16:25:34 +0200

    add model

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -9,13 +9,11 @@ from rdkit.Chem.Descriptors import MolWt
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
-# current file directory
+
 root = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(root)
 
-# my model
-def my_model(smiles_list):
-    return [MolWt(Chem.MolFromSmiles(smi)) for smi in smiles_list]
-
+from dilipred import DILIPRedictor
 
 # read SMILES from .csv file, assuming one column with header
 with open(input_file, "r") as f:
@@ -23,17 +21,17 @@ with open(input_file, "r") as f:
     next(reader)  # skip header
     smiles_list = [r[0] for r in reader]
 
-# run model
-outputs = my_model(smiles_list)
+R = []
+dp = DILIPRedictor()
+for s in smiles_list:
+    r = dp.predict(s)
+    r = r[["source", "value"]]
+    r = r.set_index('source').T
+    print(r)
+    R += [r]
 
-#check input and output have the same lenght
-input_len = len(smiles_list)
-output_len = len(outputs)
-assert input_len == output_len
 
-# write output in a .csv file
-with open(output_file, "w") as f:
-    writer = csv.writer(f)
-    writer.writerow(["value"])  # header
-    for o in outputs:
-        writer.writerow([o])
+import pandas as pd
+
+df = pd.concat(R)
+df.to_csv(output_file, index=False)
