58d5840155a0600976ed129b0a012d656e7845ea
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2025-01-04 09:55:21 +0100

    updated column names

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -2,13 +2,11 @@
 import os
 import csv
 import sys
-from rdkit import Chem
-from rdkit.Chem.Descriptors import MolWt
+import pandas as pd
 
 # parse arguments
 input_file = sys.argv[1]
 output_file = sys.argv[2]
-
 
 root = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(root)
@@ -30,8 +28,17 @@ for s in smiles_list:
     print(r)
     R += [r]
 
-
-import pandas as pd
-
+# save results to .csv file
 df = pd.concat(R)
+
+with open(os.path.join(root, "..", "columns", "run_columns.csv"), "r") as f:
+    reader = csv.reader(f)
+    next(reader)
+    columns = [r[0] for r in reader]
+
+rename = {}
+for i, c in enumerate(list(df.columns)):
+    rename[c] = columns[i]
+
+df = df.rename(columns=rename)
 df.to_csv(output_file, index=False)
